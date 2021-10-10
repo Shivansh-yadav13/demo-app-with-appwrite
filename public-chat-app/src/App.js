4f20c793e9bbf20e98ac53717b13fad68b8cdbd3
Shivansh Yadav <yadavshivansh@gmail.com>
2021-10-10 13:42:44 +0530

    auth errors

--- public-chat-app/src/App.js
+++ public-chat-app/src/App.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Signup from './Components/Signup';
@@ -17,6 +18,16 @@ function App() {
         </ Route>
       </Switch>
     </Router>
+=======
+import './App.css';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="contained">Hello World</Button>
+    </div>
+>>>>>>> parent of 85a4b97 (Added user auth using appwrite)
 =======
 import './App.css';
 import Button from '@mui/material/Button';
